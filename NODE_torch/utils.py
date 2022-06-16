9e16aef7a3c0221206edde1fb18d547f6fd5c28c
Chieh <96373449+Chieh997@users.noreply.github.com>
2022-06-16 16:12:49 +0800

    Add files via upload

--- NODE_torch/utils.py
+++ NODE_torch/utils.py
@@ -48,89 +48,14 @@ def get_cifar_dloaders(batch_size=64, size=32, path='./data/cifar10_data',downlo
     
     return trainloader, testloader
 
-class MNISTLearner(pl.LightningModule):
-    def __init__(self, model:nn.Module, trainloader, testloader):
-        super().__init__()
-        self.lr = 1e-3
-        self.model = model
-        self.iters = 0.
-        self.trainloader, self.testloader = trainloader, testloader
-    
-    def forward(self, x):
-        return self.model(x)
-    
-    def training_step(self, batch, batch_idx):
-        self.iters += 1.
-        x, y = batch   
-        # x, y = x.to(device), y.to(device)
-        # y_hat = self.model(x)\
-        self.model[1].vf.nfe = 0
-        _, trajectory = self.model[:2](x) # In ode net, the output is (shape_of_t_span, true_output )
-        y_hat = self.model[2:](trajectory[-1]) 
-        loss = nn.CrossEntropyLoss()(y_hat, y)
-        # epoch_progress = self.iters / self.loader_len
-        acc = Accuracy()(y_hat, y)
-        nfe = self.model[1].vf.nfe ; self.model[1].vf.nfe = 0
-        tqdm_dict = {
-            'train_loss': loss, 
-            'accuracy': acc, 
-            'NFE': torch.tensor(nfe, dtype=torch.float) }
-        # logs = {'train_loss': loss, 'epoch': epoch_progress}
-        self.log_dict({
-            'epoch': self.current_epoch, 
-            'train_loss': loss, 
-            'accuracy': acc, 
-            'NFE': torch.tensor(nfe, dtype=torch.float)
-            }, on_epoch=True, on_step=False)
-        # self.log_dict(tqdm_dict, on_epoch=True, on_step=False)
-        return {
-            'loss': loss, 
-            'progress_bar': tqdm_dict, 
-            # 'log': logs
-        }   
-
-    def test_step(self, batch, batch_nb):
-        x, y = batch
-        # x, y = x.to(device), y.to(device)
-        # y_hat = self(x)
-        _, trajectory = self.model[:2](x) 
-        y_hat = self.model[2:](trajectory[-1])
-        acc = Accuracy()(y_hat, y)
-        return {'test_loss': nn.CrossEntropyLoss()(y_hat, y), 'test_accuracy': acc}
-
-    def test_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        avg_acc = torch.stack([x['test_accuracy'] for x in outputs]).mean()
-        logs = {'test_loss': avg_loss}
-        self.log("test_loss", avg_loss)
-        return {
-            'avg_test_loss': avg_loss, 
-            'avg_test_accuracy': avg_acc,
-            'log': logs, 
-            'progress_bar': logs}
-    
-    def configure_optimizers(self):
-        opt = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=5e-5)
-        sched = {'scheduler': torch.optim.lr_scheduler.StepLR(opt, step_size=5, gamma = 9e-1),
-                 'monitor': 'loss', 
-                 'interval': 'step',
-                 'frequency': 10 }
-        return [opt], [sched]
-
-    def train_dataloader(self):
-        self.loader_len = len(self.trainloader)
-        return self.trainloader
-
-    def test_dataloader(self):
-        self.test_loader_len = len(self.testloader)
-        return self.testloader
-
-class CIFARLearner(pl.LightningModule):
-    def __init__(self, model:nn.Module, trainloader, testloader):
+class Learner(pl.LightningModule):
+    def __init__(self, model:nn.Module, trainloader, testloader, device):
         super().__init__()
         self.model = model
         self.iters = 0.
         self.trainloader, self.testloader = trainloader, testloader
+        self.loss = nn.CrossEntropyLoss().to(device)
+        self.accuarcy = Accuracy().to(device)
     
     def forward(self, x):
         return self.model(x)
@@ -141,9 +66,8 @@ class CIFARLearner(pl.LightningModule):
         self.model[2].vf.nfe = 0
         _, trajectory = self.model[:3](x) 
         y_hat = self.model[3:](trajectory[-1])   
-        loss = nn.CrossEntropyLoss()(y_hat, y)
-        # epoch_progress = self.iters / self.loader_len
-        acc = Accuracy()(y_hat, y)
+        loss = self.loss(y_hat, y)
+        acc = self.accuarcy(y_hat, y)
         nfe = self.model[2].vf.nfe ; self.model[2].vf.nfe = 0
         tqdm_dict = {
             'train_loss': loss, 
@@ -154,21 +78,22 @@ class CIFARLearner(pl.LightningModule):
             'train_loss': loss, 
             'accuracy': acc, 
             'NFE': torch.tensor(nfe, dtype=torch.float)
-            }, on_epoch=True, on_step=False)
+            }, on_epoch=False, on_step=True)
         return {'loss': loss, 'progress_bar': tqdm_dict}   
 
     def test_step(self, batch, batch_nb):
         x, y = batch
         _, trajectory = self.model[:3](x) 
         y_hat = self.model[3:](trajectory[-1])
-        acc = Accuracy()(y_hat, y)
-        return {'test_loss': nn.CrossEntropyLoss()(y_hat, y), 'test_accuracy': acc}
+        acc = self.accuarcy(y_hat, y)
+        logs = {'test_loss': self.loss(y_hat, y), 'test_accuracy': acc}
+        self.log_dict(logs)
+        return logs
     
     def test_epoch_end(self, outputs):
         avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
         avg_acc = torch.stack([x['test_accuracy'] for x in outputs]).mean()
         logs = {'avg_test_loss': avg_loss, 'avg_test_accuracy': avg_acc}
-        self.log_dict(logs)
         return logs
     
     def configure_optimizers(self):
